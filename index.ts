f1c120dbe4c90b07f1ff079208da6c3881b00919
Neiron-DeOrtega <igorpodelsk0@mail.ru>
2024-06-13 12:28:58 +0300

    cors changes

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express, { Application } from 'express';
 const appRouter = require('./src/Routes/appRouter')
-const cors = require('cors');
+// const cors = require('cors');
 
 const app: Application = express();
 const PORT = 4000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use('/manager', appRouter)
